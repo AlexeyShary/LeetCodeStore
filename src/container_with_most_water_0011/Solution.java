3b4c3b7f31cf4ce2faf65b348f0cfad8916dcd85
Alexey Shary <badirbis@yandex.ru>
2024-06-04 09:43:34 +0300

    0011 - Container with most water

---
+++ src/container_with_most_water_0011/Solution.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/container-with-most-water/
+package container_with_most_water_0011;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int result = 0;
+
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            result = Math.max(result, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
