c041d6e943cdbd4d4d551d0bfa9bc9b5ebbf053e
Alexey Shary <badirbis@yandex.ru>
2024-06-08 17:11:20 +0300

    0523 - Continuous Subarray Sum

---
+++ src/continuous_subarray_sum_0523/Solution.java
@@ -0,0 +1,29 @@
+package continuous_subarray_sum_0523;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> remainderIndexMap = new HashMap<>();
+        int sum = 0;
+        remainderIndexMap.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (k != 0) {
+                sum %= k;
+            }
+
+            if (remainderIndexMap.containsKey(sum)) {
+                if (i - remainderIndexMap.get(sum) > 1)
+                    return true;
+            } else {
+                remainderIndexMap.put(sum, i);
+            }
+        }
+
+        return false;
+    }
+}
